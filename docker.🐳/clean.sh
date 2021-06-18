cc006ea2e9e10c0a67e2b332e8c3064a3512c7ef
Brianh sm311s11k3s0ld3r <b+sm3lly@elastic.ventures>
2021-06-18 16:40:59 +1000

    fzf docker menu

---
+++ docker.🐳/clean.sh
@@ -0,0 +1,15 @@
+ # _b00t_ docker clean/tidy
+ 
+
+# https://forums.docker.com/t/how-to-remove-none-images-after-building/7050
+d1ngL3=`docker images -f "dangling=true" -q | wc -l`
+if [ "$d1ngL3_本" -gt 0 ] ; then
+    log_📢_记录 "🐳 d1ngL3_本 is:$d1ngL3_本"
+
+    # clean all
+    docker image rm `docker images -f "dangling=true" -q`
+
+    # clean-selective with fzf
+    docker image rm `docker images -f "dangling=true" -q | fzf`
+
+fi 
