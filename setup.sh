9ad894e1677d719a9aac16106f004aaeb3e4aadc
Brian H <brianh@elastic.ventures>
2024-07-04 00:09:47 +0000

    sealed secrets

--- setup.sh
+++ setup.sh
@@ -146,3 +146,10 @@ source $HOME/.bash_profile
 )
 
 kubectl krew install cilium
+
+## kubeseal
+## https://github.com/bitnami-labs/sealed-secrets
+KUBESEAL_VERSION='' # Set this to, for example, KUBESEAL_VERSION='0.23.0'
+curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
+tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
