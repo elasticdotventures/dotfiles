0e70d78bf5e1ca3e5d8a6f2db1e216c7b2552e15
elasticdotventures <brianh@elastic.ventures>
2025-06-26 13:54:37 +0000

    fix: Use GITHUB_TOKEN directly in setup.sh and remove GH_TOKEN build arg

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ if [ "$IS_CI" = false ]; then
   gh extension install github/gh-copilot --force
 elif [ -n "$GH_TOKEN" ]; then
   echo "Attempting to log in to GitHub CLI using provided GH_TOKEN"
-  echo "$GH_TOKEN" | gh auth login --with-token
+  echo "${GITHUB_TOKEN}" | gh auth login --with-token
   gh extension install https://github.com/nektos/gh-act
   gh extension install github/gh-copilot --force
 fi
