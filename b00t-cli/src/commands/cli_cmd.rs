099aa1daf181666e098e08c0365d9036f94fadfc
elasticdotventures <brianh@elastic.ventures>
2025-07-30 12:33:52 +0000

    fix: bad merge

--- b00t-cli/src/commands/cli_cmd.rs
+++ b00t-cli/src/commands/cli_cmd.rs
@@ -2,7 +2,6 @@ use anyhow::Result;
 use clap::Parser;
 use crate::datum_cli::CliDatum;
 use crate::traits::*;
-use crate::load_datum_providers;
 use crate::{get_expanded_path, load_datum_providers};
 use duct::cmd;
 // use std::fs;
